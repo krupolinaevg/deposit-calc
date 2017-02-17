85d1d05b14064c32fb22206719a8ac7a9b3c6a98
Крупницкая Полина Евгеньевна <ri68s08@mwp08.csc.local>
2017-02-17 18:28:46 +0700

    Edited for code style

--- l2.c
+++ l2.c
@@ -1,49 +1,46 @@
 #include<stdio.h>	
 	
 	
-int vv(int v)
-{
-	if(v<10)
-	printf("ERROR");
-	return v;
-}
+int vv(int v){
+    if(v<10)
+    printf("ERROR");
+    return 1;
+    }
 
+int ss(int s, int v){
 
-int ss(int s, int v)
-{
-	int o;
+    int o;
 	
-	if(v<100){
-	if(s<0){
-	printf("ERROR");
-	}
-	if(s<30){
-	o=v*0.9;
-	}
-	if((s<120)&&(s>30)){
-	o=v*1.02;
-	}
-	if((s<240)&&(s>120)){
-	o=v*1.06;
-	}
-	if((s>240)&&(s<366)){
-	o=v*1.12;
-	}
+    if(v<100){
+        if(s<0){
+        printf("ERROR");
+        }
+        if(s<30){
+            o=v*0.9;
+	    }
+	    if((s<120)&&(s>30)){
+            o=v*1.02;
+	    }
+        if((s<240)&&(s>120)){
+            o=v*1.06;
+        }
+        if((s>240)&&(s<366)){
+            o=v*1.12;
+        }
 	}	 
 	if(v>100){
-	if(s<30){
-	o=v*0.9;
-	}
-	if((s<120)&&(s>30))
-	{
-	o=v*1.02;
-	}
-	if((s<240)&&(s>120)){
-	o=v*1.06;
-	}
-	if((s>240)&&(s<366)){
-	o=1.12;
-	}
+        if(s<30){
+            o=v*0.9;
+        }
+        if((s<120)&&(s>30)){
+            o=v*1.02;
+        }
+        if((s<240)&&(s>120)){
+            o=v*1.06;
+        }
+        if((s>240)&&(s<366)){
+            o=1.12;
+    }
 	}
 	return o;
 }		
