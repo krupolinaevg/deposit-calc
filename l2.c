a7d7885b0acb0bc086ffaf8594edd06822759108
Крупницкая Полина Евгеньевна <ri68s08@mwp08.csc.local>
2017-02-17 18:05:18 +0700

    Osnova

---
+++ l2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>	
+	
+	
+	
+int main()
+{
+	int v,s;
+	int vvv,sss;
+		
+	printf("Vvedite razmer vklada(v tisichax):");
+	scanf("%d",&v);
+	printf("Vvedite srok(v dnyah):");
+	scanf("%d",&s);
+	
+	vvv=vv(v);
+	printf("%d\n",vvv);
+	
+	sss=ss(s,v);
+	printf("%d\n",sss);
+	
+	return 0;
+}
