6b72f85fb138dff2fcab4d6600472aae659b0f21
Крупницкая Полина Евгеньевна <ri68s08@mwp11.csc.local>
2017-03-20 14:27:01 +0700

    Check

--- l2.c
+++ l2.c
@@ -1,44 +1,46 @@
 #include<stdio.h>	
 	
 	
-int vv(int v){
-    if(v<10)
+int vv(int* v){
+    if(*v<10)
     printf("ERROR");
     return 1;
     }
 
-int ss(int s, int v){
+int ss(int* s, int* v){
 
     int o;
 	
-    if(v<100){
-        if(s<0){
+	if(*v<100){
+        if(*s<0){
         printf("ERROR");
         }
-        if(s<30){
-            o=v*0.9;
+        if(*s<30){
+            o=*v*0.9;
+            
 	    }
-	    if((s<120)&&(s>30)){
-            o=v*1.02;
+	    if((*s<120)&&(*s>30)){
+            o=*v*1.02;
 	    }
-        if((s<240)&&(s>120)){
-            o=v*1.06;
+        if((*s<240)&&(*s>120)){
+            o=*v*1.06;
         }
-        if((s>240)&&(s<366)){
-            o=v*1.12;
+        if((*s>240)&&(*s<366)){
+            o=*v*1.12;
         }
-	}	 
-	if(v>100){
-        if(s<30){
-            o=v*0.9;
         }
-        if((s<120)&&(s>30)){
-            o=v*1.02;
+		 
+	if(*v>100){
+        if(*s<30){
+            o=*v*0.9;
         }
-        if((s<240)&&(s>120)){
-            o=v*1.06;
+        if((*s<120)&&(*s>30)){
+            o=*v*1.02;
         }
-        if((s>240)&&(s<366)){
+        if((*s<240)&&(*s>120)){
+            o=*v*1.06;
+        }
+        if((*s>240)&&(*s<366)){
             o=1.12;
     }
 	}
@@ -54,10 +56,10 @@ int main()
 	printf("Vvedite srok(v dnyah):");
 	scanf("%d",&s);
 	
-	vvv=vv(v);
+	vvv=vv(&v);
 	printf("%d\n",vvv);
 	
-	sss=ss(s,v);
+	sss=ss(&s,&v);
 	printf("%d\n",sss);
 	
 	return 0;
